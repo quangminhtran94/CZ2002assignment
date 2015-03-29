ffa6e080c1cded52767ece63fbd60af07659a3f4
elverson <lixuebin19@hotmail.com>
2015-03-30 03:00:15 +0800

    Update Tutorial.java

--- src/Entity/Tutorial.java
+++ src/Entity/Tutorial.java
@@ -1,5 +1,38 @@
-package Entity;
+package tutorial;
+import java.util.ArrayList;
 
-public class Tutorial extends CourseComponent {
+public class Tutorial implements java.io.Serializable {
 
-}
+	
+		private int tutorialId;
+		private String tutorialName;
+		private ArrayList<Tutorial> tutorial;
+		
+		public Tutorial(String tutorialName, int tutorialId){
+			this.tutorialId = tutorialId;
+			this.tutorialName = tutorialName;
+			
+		}
+
+	    public String getTutorialName(){
+	    	
+	    	return tutorialName;
+	    }
+	    
+	    public void setTutorialName(String tutorialName){
+	    	
+	    	this.tutorialName = tutorialName;
+	    }
+	 
+	    public int getTutorialId(){
+	    	
+	    	return tutorialId;
+	    }
+	    
+	    public void setTutorialId(int tutorialId){
+	    	
+	    	this.tutorialId = tutorialId;
+	    	
+	    }
+	}
+
