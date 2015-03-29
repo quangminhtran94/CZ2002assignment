1b9e7b54f8af064ae9116eed4024d2a9967beba7
elverson <lixuebin19@hotmail.com>
2015-03-30 02:54:09 +0800

    Update Lab.java

--- src/Entity/Lab.java
+++ src/Entity/Lab.java
@@ -1,5 +1,40 @@
-package Entity;
+package lab;
+import java.util.ArrayList;
 
-public class Lab extends CourseComponent {
+public class Lab implements java.io.Serializable {
+
+	private int labId;
+	private String labName;
+	private ArrayList<Lab> lab;
+	
+
+	public Lab(String labName, int labId){
+		
+		this.labId = labId;
+		this.labName = labName;
+		
+	}
+		 public String labName(){
+		    	
+		    	return labName;
+		    }
+		    
+		    public void setLabName(String labName){
+		    	
+		    	this.labName = labName;
+		    }
+		 
+		    public int getlabId(){
+		    	
+		    	return labId;
+		    }
+		    
+		    public void setlabId(int labId){
+		    	
+		    	this.labId = labId;
+		    	
+		    }
+		    
+	
 
 }
